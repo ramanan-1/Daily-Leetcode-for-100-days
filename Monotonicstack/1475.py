2a5b8dadacdf1af4969163b670be2fcfa723e290
ramanan-1 <211810143+ramanan-1@users.noreply.github.com>
2025-05-22 20:33:05 +0530

    Monotonic

--- Monotonicstack/1475.py
+++ Monotonicstack/1475.py
@@ -1,7 +1,19 @@
-def finalPrices(prices):
+def finalPrices(self, prices: List[int]) -> List[int]:
     
         discount = 0
         stack = []
         result = [0] * len(prices)
 
-        
+        for idx in range(len(prices)-1,-1,-1) :
+            price = prices[idx]
+            
+            while stack and stack[-1] > price :
+                stack.pop()
+
+            if stack :
+                discount = price - stack[-1]
+                result[idx] = discount
+            else:
+                result[idx] = price
+            stack.append(price)
+        return result 
