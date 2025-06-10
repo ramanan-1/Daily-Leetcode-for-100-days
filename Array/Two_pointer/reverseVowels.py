62221c4f2bf95a4e72da3ad69421e780269d8ff2
ramanan-1 <211810143+ramanan-1@users.noreply.github.com>
2025-06-10 23:09:46 +0530

    Reverse vowels

---
+++ Array/Two_pointer/reverseVowels.py
@@ -0,0 +1,22 @@
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+
+        vowels = set('AEIOUaeiou')
+
+        s = list(s)
+      
+        l = 0
+        r = len(s)-1
+
+        while l < r :
+            if s[l] not in vowels :
+                l+=1
+            elif s[r] not in vowels:
+                r-=1
+            else:
+                s[l],s[r] = s[r],s[l]
+                l+=1
+                r-=1
+        return ''.join(s)
+        
+        
