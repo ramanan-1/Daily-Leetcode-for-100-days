c429fa9b82c77ed7ef0ceb4f3c012e7e417cb0ae
ramanan-1 <211810143+ramanan-1@users.noreply.github.com>
2025-06-15 19:17:42 +0530

    Solved

---
+++ LinkedList/83.py
@@ -0,0 +1,10 @@
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        temp = head
+        while temp and temp.next :
+            if temp.val == temp.next.val:
+                temp.next = temp.next.next
+            else:
+                temp = temp.next
+        return head
